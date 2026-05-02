50b0f8a0b0c30dcd448e506737c02c5ce561612c
Ravi Nadahar <nadahar@rediffmail.com>
2026-05-02 06:26:02 +0200

    ID -> UID
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.ui/web/src/components/rule/rule-general-settings.vue
+++ bundles/org.openhab.ui/web/src/components/rule/rule-general-settings.vue
@@ -5,7 +5,7 @@
         <f7-list inline-labels no-hairlines-md>
           <f7-list-input
             ref="ruleId"
-            :label="`${type} ID`"
+            :label="`${type} UID`"
             type="text"
             :placeholder="`A unique identifier for the ${type.toLowerCase()}`"
             :value="rule.uid || ''"
@@ -67,7 +67,7 @@
       <f7-col class="skeleton-text skeleton-effect-blink">
         <f7-list inline-labels no-hairlines-md>
           <f7-list-input
-            label="Rule ID"
+            :label="`${type} UID`"
             type="text"
             placeholder="Required"
             value="_______"
